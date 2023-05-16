3e79931c2afc2963407ec45fe9aebbbd9979e0eb
litao <tao.li396@imaygou.com>
2023-05-16 19:18:03 +0800

    ADD:init

---
+++ backend/src/main/java/com/astocoding/backend/fileSource/FileSource.java
@@ -0,0 +1,14 @@
+package com.astocoding.backend.fileSource;
+
+/**
+ * @author stuart
+ * @since 2023/5/16 19:01
+ * @desc fileSourceInterface
+ */
+public interface FileSource {
+    /**
+     * fileSource
+     * 保存文件体系属性，配置信息，权限，key等数据
+     * 
+     */
+}
